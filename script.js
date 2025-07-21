403eb353e3a0224b2b9878eb14e4245671ed6702
SSJdeveloper99 <sabirjanovsanjar39@gmail.com>
2025-07-21 10:04:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let text = prompt("ismingizni yozing")
+let number = prompt("Yoshingizni kiriting")
+let boolean = prompt("siz boshqatdan isbotlang")
+
+
+let resultt = alert("Sizni " + text + " so'zi  " + (typeof text) + "ismingiz" + 
+"\nSizni" + number + " so'zi  " + (typeof 123) + "yoshingiz" + 
+"\nSiz kiritgan " + boolean + " so'zi  " + (typeof true) + " typega tegishli");
